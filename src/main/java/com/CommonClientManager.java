146e3a88b6ce3ed3c5040ced44d2a758b5193881
chuyenns <chuyenns@d2t.vn>
2021-07-03 17:04:53 +0700

    Update code

---
+++ src/main/java/com/CommonClientManager.java
@@ -0,0 +1,29 @@
+package com;
+
+import it.tdlight.common.*;
+
+public abstract class CommonClientManager {
+
+	private static InternalClientManager getClientManager(String implementationName) {
+		// ClientManager is singleton:
+		return InternalClientManager.get(implementationName);
+	}
+
+	protected synchronized static TelegramClient create(String implementationName) {
+		InternalClient client = new InternalClient(getClientManager(implementationName));
+		return create(client);
+	}
+
+	protected synchronized static ReactiveTelegramClient createReactive(String implementationName) {
+		InternalReactiveClient reactiveClient = new InternalReactiveClient(getClientManager(implementationName));
+		return createReactive(reactiveClient);
+	}
+
+	private static TelegramClient create(InternalClient internalClient) {
+		return internalClient;
+	}
+
+	private static ReactiveTelegramClient createReactive(InternalReactiveClient internalReactiveClient) {
+		return internalReactiveClient;
+	}
+}
